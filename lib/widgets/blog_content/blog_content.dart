8320685957318a24396bb6eec53fc0ddcba6c917
jk03-legend <joshuasalcedo899@gmail.com>
2026-07-19 15:52:09 +0800

    Responsiveness

--- lib/widgets/blog_content/blog_content.dart
+++ lib/widgets/blog_content/blog_content.dart
@@ -1,25 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class BlogContent extends StatelessWidget {
   const BlogContent({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 600,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('SHARE YOUR STORY', style: TextStyle(height: 0.9, fontWeight: FontWeight.w800, fontSize: 80),),
-          SizedBox(height: 30),
-          Text('This is the content of the blog post. It can be multiple paragraphs long and contain various information about the topic being discussed.', 
-          style: TextStyle(
-            fontSize: 21, 
-            height: 1.7)
-          ),
-        ],
-      ),
-    );
+      return ResponsiveBuilder(
+        builder: (context, sizingInformation) {
+          var textAlignment = sizingInformation.deviceScreenType == DeviceScreenType.desktop
+              ? TextAlign.left
+              : TextAlign.center;
+          double titleSize = sizingInformation.deviceScreenType == DeviceScreenType.mobile
+              ? 50
+              : 80;
+          double descriptionSize = sizingInformation.deviceScreenType == DeviceScreenType.mobile
+              ? 16
+              : 21;
+
+          return SizedBox(
+            width: 600,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('SHARE YOUR STORY', style: TextStyle(height: 0.9, fontWeight: FontWeight.w800, fontSize: titleSize),),
+                SizedBox(height: 30),
+                Text('This is the content of the blog post. It can be multiple paragraphs long and contain various information about the topic being discussed.', 
+                style: TextStyle(
+                  fontSize: descriptionSize, 
+                  height: 1.7),
+                  textAlign: textAlignment,
+                ),
+                
+              ],
+            ),
+          );
+        }
+      );
   }
 }
