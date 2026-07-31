d1e9963bc4e132ad70bee4ecb40e43930ef6aae8
jk03-legend <joshuasalcedo899@gmail.com>
2026-07-31 18:16:10 +0800

    Logout button for mobile mode

--- lib/widgets/navigation_drawer/navigation_drawer.dart
+++ lib/widgets/navigation_drawer/navigation_drawer.dart
@@ -1,3 +1,4 @@
+import 'package:blog_site/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:blog_site/widgets/navigation_drawer/navigation_drawer_header.dart';
@@ -35,6 +36,27 @@ class NavDrawer extends StatelessWidget {
             onTap: () {
               context.pop();
               context.go("/profile");
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text("Logout"),
+            onTap: () async {
+              try{
+                await AuthService().logout();
+
+                if (!context.mounted) return;
+                context.pop();
+                context.go("/login");
+              }
+              catch (e){
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Failed to logout: $e'),
+                  ),
+                );
+                debugPrint("Failed to logout: $e");
+              }
             },
           ),
             ]
